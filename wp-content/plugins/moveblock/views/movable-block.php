a495d756543235f1ecf813bf2956ea1d3f384655
alpha <alpha@localhost>
2017-08-08 15:24:47 +0300

    Add Lorem ipsum

--- wp-content/plugins/moveblock/views/movable-block.php
+++ wp-content/plugins/moveblock/views/movable-block.php
@@ -13,7 +13,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="description" content="">
-    <meta name="google-site-verification" content="_V4FadjizuJnCsVqMuFi3kSwS387xuQMu1GeaWacW-I">
+    <meta name="google-site-verification" content="">
 
     <title>Youngseaser SEO Analyze tool</title>
 
@@ -44,7 +44,6 @@
                     return position;
                 }
             });
-            // $(".tooltipster").tooltipster().tooltipster("open");
         });
     </script>
 
@@ -63,23 +62,13 @@
         }
     </style>
     <link rel="stylesheet" id="validation_css-css" href="<?php echo plugin_dir_url( __FILE__ ).'assets/';?>css/jquery.validate.css" type="text/css" media="all">
-    <link rel="stylesheet" id="youngseaser-hp-normalize-css" href="<?php echo plugin_dir_url( __FILE__ ).'assets/';?>css/page-home-normalize.css" type="text/css" media="all">
-    <link rel="stylesheet" id="youngseaser-hp-css" href="<?php echo plugin_dir_url( __FILE__ ).'assets/';?>css/page-home-sidebar.css" type="text/css" media="all">
+    <link rel="stylesheet" id="hp-normalize-css" href="<?php echo plugin_dir_url( __FILE__ ).'assets/';?>css/page-home-normalize.css" type="text/css" media="all">
+    <link rel="stylesheet" id="hp-css" href="<?php echo plugin_dir_url( __FILE__ ).'assets/';?>css/page-home-sidebar.css" type="text/css" media="all">
     <script type="text/javascript" src="<?php echo plugin_dir_url( __FILE__ ).'assets/';?>js/jquery.js"></script>
     <script type="text/javascript" src="<?php echo plugin_dir_url( __FILE__ ).'assets/';?>js/jquery-migrate.min.js"></script>
     <script type="text/javascript" src="<?php echo plugin_dir_url( __FILE__ ).'assets/';?>js/jquery.validate.js"></script>
     <!--[if lt IE 9]>
-    <script type='text/javascript' src='https://youngseaser-wpengine.netdna-ssl.com/wp-content/themes/genesis/lib/js/html5shiv.js?ver=3.7.3'></script>
     <![endif]-->
-
-    <link rel="icon" href="<?php echo plugin_dir_url( __FILE__ ).'assets/';?>favicon.ico">
-    <!-- Optimizely testing snippet -->
-    <script src="js/205470252.js"></script>
-
-
-
-    <!--[if lt IE 9]><script src="https://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
-
 
 </head>
 
@@ -90,8 +79,8 @@
         <div class="icons">
             <div class="phone"></div>
             <div class="container">
-            <div class="container-inner">
-                <div class="envelope"></div>
+                <div class="container-inner">
+                    <div class="envelope"></div>
                 </div>
             </div>
             <div class="cloud"></div>
@@ -99,81 +88,71 @@
     </div>
 
     <div class="content-flow">
-        <h2>Here’s how Quick Sprout works</h2>
-        <p class="lead"><strong>Quick Sprout</strong> makes growing your website traffic dead-simple.</p>
+        <h2>Lorem ipsum</h2>
 
         <ul>
             <li>
                 <img src="fonts/graphic-1.svg" alt="Connect your Google Analytics account with Quick Sprout">
-                <h3>Make sense of Google Analytics</h3>
-                <p>Quick Sprout analyzes your website traffic by connecting to your Google Analytics account.</p>
+                <h3>Where does it come from?</h3>
+                <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+
+                    The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
             </li>
             <li>
-                <img src="images/graphic-2.png" alt="Quick Sprout alert">
-                <h3>Personalized website alerts</h3>
-                <p>Get easy-to-understand alerts every time there are website improvements that you should make.</p>
+                <h3>Where can I get some?</h3>
+                <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
+
+                </p>
             </li>
             <li>
-                <img src="fonts/graphic-3.svg" alt="Quick Sprout traffic progress">
-                <h3>Grow alongside your traffic</h3>
-                <p>Each step you take to grow your website traffic is a one step closer to learning about your customers and what they want.</p>
+                <h3>Why do we use it?</h3>
+                <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
             </li>
         </ul>
     </div>
 
     <div class="content-overview">
         <div class="details">
-            <h2>Meet the Quick Sprout platform</h2>
-            <p class="lead">Quick Sprout helps you cut to the chase when growing your website traffic by showing you what improvements are important.</p>
+            <h2>Lorem Ipsum</h2>
+            <p class="lead">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+            </p>
             <ul>
                 <li>
-                    <img src="fonts/list-icon-1.svg">
                     <span>
-              <h3>Make human decisions.</h3>
-              <p>Your analytics data is more than numbers &amp; graphs. Celebrate your achievements with a clear view of what’s happening on your website.</p>
+              <h3>Lorem Ipsum</h3>
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+              </p>
             </span>
                 </li>
-                <li>
-                    <img src="fonts/list-icon-2.svg">
-                    <span>
-              <h3>Be alerted of important changes.</h3>
-              <p>When a traffic improvement is available, Quick Sprout sends you an alert so you can act and make the change quickly.</p>
-            </span>
+                <h3>Lorem Ipsum</h3>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+                </p>
+                </span>
                 </li>
                 <li>
                     <img src="fonts/list-icon-3.svg">
                     <span>
-              <h3>Grow. Learn. Repeat.</h3>
-              <p>Built from Neil Patel’s marketing knowledge, Quick Sprout helps you learn the what, why, and how, while continually growing your traffic.</p>
+              <h3>Lorem Ipsum</h3>
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+              </p>
             </span>
                 </li>
             </ul>
         </div>
-        <div class="graphic">
-            <img src="images/screenshot.png" alt="Screenshot of Quick Sprout interface">
-        </div>
     </div>
 
     <div class="content-cta">
-        <h2>Sign up to make even more improvements</h2>
-        <p class="lead">Quick Sprout is the easiest way for you to know what is happening on your website and how to improve your website to grow your traffic.</p>
-        <p class="cta">
-            <a href="https://app.youngseaser.com/signup" class="btn">Get Started</a>
-            <span>Quick Sprout will always be free.
-      </span></p>
+        <h2>Lorem Ipsum</h2>
+        <p class="lead">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        </p>
     </div>
 
-    <footer>
-        <a class="logo" href="https://youngseaser.com" title="Welcome to Quick Sprout"><img src="fonts/logo-youngseaser-alt.svg" height="30" alt="Quick Sprout Logo"></a>
-        <nav>
-            <a href="/terms/">Terms of Service</a>
-            <a href="/privacy/">Privacy Policy</a>
-            <a href="http://changelog.youngseaser.com/">Changelog</a>
-        </nav>
-    </footer>
 </div>
 
-<footer id="footer" role="contentinfo">Copyright &copy; by Quick Sprout, LLC</footer>
 
 <script type="text/javascript">
     /* <![CDATA[ */
